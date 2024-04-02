fb03a1f67719620ef38ca74a789071f63ed03ed3
mart0valov <martin.valov98@gmail.com>
2024-04-02 13:17:23 +0300

    implementing basic crud operations

--- photo-gallery-api/src/index.js
+++ photo-gallery-api/src/index.js
@@ -29,23 +29,49 @@ app.post('/api/create', async (request, response) => {
 app.get('/api/photos', async (request, response) => {
     try {
         const allPhotos = await Photo.find();
+        if (!photo) throw new Error(`Currently there are no photos uploaded`);
         return response.status(200).send(allPhotos);
     } catch (err) {
-        console.log(err)
+        console.log(err);
+        return response.status(404).send({ msg: err.message });
     }
 });
 
 // retrieve single photo by id
-app.get('/api/photo/:id', (request, response) => {
-
+app.get('/api/photo/:id', async (request, response) => {
+    const photoId = request.url.split('/')[3];
+    try {
+        const photo = await Photo.findById(photoId);
+        if (!photo) throw new Error(`Photo with id ${photoId} not found`);
+        return response.status(200).send(photo);
+    } catch (err) {
+        return response.status(404).send({ msg: err.message });
+    }
 });
 // update photo by id
-app.put('/api/photo/:id', (request, response) => {
+app.put('/api/photo/:id', async (request, response) => {
+    const photoId = request.url.split('/')[3];
+    console.log(photoId);
+    const { body } = request;
 
+    try {
+        const photo = await Photo.findByIdAndUpdate(photoId, { ...body });
+        if (!photo) throw new Error(`Photo with id ${photoId} not found`);
+        return response.status(200).send({ msg: "Photo edited successfully" });
+    } catch (err) {
+        return response.status(404).send({ msg: err.message });
+    }
 });
 // delete photo by id
-app.delete('/api/photo/:id', (request, response) => {
-
+app.delete('/api/photo/:id', async (request, response) => {
+    const photoId = request.url.split('/')[3];
+    try {
+        const photo = await Photo.findByIdAndDelete(photoId);
+        if (!photo) throw new Error(`Photo with id ${photoId} not found`);
+        return response.status(200).send({ msg: "Photo deleted successfully" });
+    } catch (err) {
+        return response.status(404).send({ msg: err.message });
+    }
 });
 
 app.listen(PORT, () => {
