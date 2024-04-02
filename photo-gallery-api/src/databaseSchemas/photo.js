1abd210ec83bbc8c6a971aa3254b77d987d150b1
mart0valov <martin.valov98@gmail.com>
2024-04-02 12:50:50 +0300

    implementing creation and retrieving of all photos from the databse

---
+++ photo-gallery-api/src/databaseSchemas/photo.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+const PhotoSchema = new mongoose.Schema({
+    title: {
+        type: mongoose.Schema.Types.String,
+        required: true
+    },
+    description: {
+        type: mongoose.Schema.Types.String,
+        required: true
+    }
+    // TODO: implementing field for local photo url
+});
+
+export const Photo = mongoose.model("Photo", PhotoSchema);
--- photo-gallery-api/src/index.js
+++ photo-gallery-api/src/index.js
@@ -1,14 +1,38 @@
 import express from "express";
+import mongoose from "mongoose";
+import { Photo } from "./databaseSchemas/photo.js";
+
 const PORT = 3000;
 const app = express();
 
+app.use(express.json());
+
+mongoose.connect('mongodb://localhost/photo-gallery-api-db')
+    .then(() => console.log('Connected to database'))
+    .catch((err) => console.log(err));
+
 // create and uplaod new photo
-app.post('/api/create', (request, response) => {
+app.post('/api/create', async (request, response) => {
+    const { body } = request;
+    const newPhoto = new Photo(body);
+
+    try {
+        const savedPhoto = await newPhoto.save();
+        return response.status(201).send({ msg: "Success", photoId: savedPhoto._id });
+    } catch (err) {
+        console.log(err);
+        return response.status(400).send({ msg: "error occured" });
+    }
 
 });
 // retrieve all photos
-app.get('/api/photos', (request, response) => {
-
+app.get('/api/photos', async (request, response) => {
+    try {
+        const allPhotos = await Photo.find();
+        return response.status(200).send(allPhotos);
+    } catch (err) {
+        console.log(err)
+    }
 });
 
 // retrieve single photo by id
